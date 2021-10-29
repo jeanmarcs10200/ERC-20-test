eed1286067f3f49e3ea38e53653f02daaec28a37
jeanmarcs10200 <jeanmarcos102008@gmail.com>
2021-10-29 19:40:02 +0200

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,10 +1,14 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts@4.3.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CoinFiance is ERC20 {
+contract MyToken is ERC20, Ownable {
     constructor() ERC20("CoinFiance", "BCS") {
         _mint(msg.sender, 500000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
